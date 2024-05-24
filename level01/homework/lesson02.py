9d0b81530f940060ce53d7b0f225e15b0c430d43
tengo122 <nemsadzetnego@gmail.com>
2024-05-24 20:32:48 +0400

    first commit

---
+++ level01/homework/lesson02.py
@@ -0,0 +1,38 @@
+from turtle import *
+speed(30)
+#saxlis kedlebi
+color("blue")
+width(7)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+#karebi
+penup()
+color("yellow")
+forward(80)
+left(90)
+pendown()
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+#saxuravi
+penup()
+goto(200, 200)
+color("red")
+right(130)
+pendown()
+forward(120)
+left(90)
+forward(120)
+
+
+
+
+exitonclick()
